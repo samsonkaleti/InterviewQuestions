4d2acf7e341d60119fdac8580fa090166d9647cc
samsonkaleti <kaletishyam@gmail.com>
2025-03-03 00:00:49 +0530

    Add basic programming examples in Day-1-BasicProgramming.js

---
+++ DSA/Day-1-BasicProgramming.js
@@ -0,0 +1,36 @@
+// 03-02-2025
+
+// console.log("1" + 2); // 12
+// const a = "23" 
+
+// const b = Number(a) 
+// console.log(b + 2); // 25
+
+
+// Methos - 1 
+
+let a = 10 
+let b = 20 
+
+let c; 
+c = a  // c = 10 
+a = b  // a = 20
+b = c // b = 10  
+console.log(a, b); // 20 10
+
+// Methos - 2
+a = a + b  // a = 30 
+
+b = a - b  // b = 10 
+a = a - b // a = 20 
+console.log(a, b); // 20 10
+
+// Methos - 3
+[a,b] = [b,a]
+console.log(a, b); // 20 10
+
+
+
+
+console.log(Math.floor((2/3))); 
+
